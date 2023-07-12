350c8197c235ec4565bbef4174cf6d9d34bdf2fc
fandri.yunus <fandri.yunus@gmail.com>
2023-07-12 16:17:51 +0800

    create layout structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,62 @@
 import "./App.scss";
+import "./styles/global.scss";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Users from "./pages/users/Users";
+import Products from "./pages/products/Products";
+import Navbar from "./componets/navbar/Navbar";
+import Footer from "./componets/footer/Footer";
+import Menu from "./componets/menu/Menu";
+import Login from "./pages/login/Login";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  const Layout = () => {
+    return (
+      <div className="main">
+        <Navbar />
+        <div className="container">
+          <div className="menu-container">
+            <Menu />
+          </div>
+          <div className="content-container">
+            <Outlet />
+          </div>
+        </div>
+        <Footer />
+      </div>
+    );
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/home",
+          element: <Home />,
+        },
+        {
+          path: "/users",
+          element: <Users />,
+        },
+        {
+          path: "/products",
+          element: <Products />,
+        },
+      ],
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/componets/navbar/Navbar.tsx
@@ -0,0 +1,14 @@
+import "./navbar.scss";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="log">
+        <img src="logo.svg" alt="" />
+        <span>bahafa</span>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
